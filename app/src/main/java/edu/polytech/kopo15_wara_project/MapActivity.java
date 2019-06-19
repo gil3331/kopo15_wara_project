b550f7002f9477c86d7667ddda33bf74533711d0
leegiljoon <leegiljoon@gmail.com>
2019-06-19 17:04:47 +0900

    little edit login layout(renamed button name)

--- app/src/main/java/edu/polytech/kopo15_wara_project/MapActivity.java
+++ app/src/main/java/edu/polytech/kopo15_wara_project/MapActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
+
 import net.daum.mf.map.api.MapView;
 
 public class MapActivity extends Activity {
@@ -12,10 +13,15 @@ public class MapActivity extends Activity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_map);
             MapView mapView = new MapView(this);
-
             ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
             mapViewContainer.addView(mapView);
 
+
+//            public void setCurrentLocationEventListener(MapView.CurrentLocationEventListener currentLocationEventListener)
+//            mMapView = (MapView) findViewById(R.id.map_view);
+//            mMapView.setDaumMapApiKey(API_KEY);
+//            mMapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+//            mMapView.setCurrentLocationEventListener(mCurrentLocationEventListener);
         }
 
 }
